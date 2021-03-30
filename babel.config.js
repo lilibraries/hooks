00165cbda328d7884918f7997d1a6bd0053c8c55
李蔚生 <971353677@qq.com>
2021-03-30 20:33:55 +0800

    test: use different babel config in the test env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+const isTest = env === "test";
+
 module.exports = {
-  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+  presets: isTest
+    ? ["./configs/babel.preset.js"]
+    : [require.resolve("@docusaurus/core/lib/babel/preset")],
 };
