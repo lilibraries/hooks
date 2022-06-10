487ea23f34a9e31e96044cda9d707508abbb7a3c
李蔚生 <971353677@qq.com>
2022-06-10 22:57:24 +0800

    test(useUpdate): check tests

--- tests/useUpdate.test.ts
+++ tests/useUpdate.test.ts
@@ -2,27 +2,30 @@ import { useUpdate } from "@lilib/hooks";
 import { renderHook } from "@testing-library/react-hooks";
 
 describe("useUpdate", () => {
-  it("should run effect callback after the component is updated without deps", () => {
+  it("should run effect after the component is updated without deps", () => {
     const destroy = jest.fn();
     const create = jest.fn().mockReturnValue(destroy);
     const { rerender, unmount } = renderHook(() => {
       useUpdate(create);
     });
 
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     rerender();
     expect(create).toBeCalledTimes(1);
-    expect(destroy).toBeCalledTimes(0);
+    expect(destroy).not.toBeCalled();
+
     rerender();
     expect(create).toBeCalledTimes(2);
     expect(destroy).toBeCalledTimes(1);
+
     unmount();
     expect(create).toBeCalledTimes(2);
     expect(destroy).toBeCalledTimes(2);
   });
 
-  it("should run effect callback after the deps are changed", () => {
+  it("should run effect after the deps are changed", () => {
     const destroy = jest.fn();
     const create = jest.fn().mockReturnValue(destroy);
     const { rerender, unmount } = renderHook(
@@ -32,42 +35,50 @@ describe("useUpdate", () => {
       { initialProps: [0] }
     );
 
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     rerender([0]);
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     rerender([1]);
     expect(create).toBeCalledTimes(1);
-    expect(destroy).toBeCalledTimes(0);
+    expect(destroy).not.toBeCalled();
+
     rerender([2]);
     expect(create).toBeCalledTimes(2);
     expect(destroy).toBeCalledTimes(1);
+
     rerender([2]);
     expect(create).toBeCalledTimes(2);
     expect(destroy).toBeCalledTimes(1);
+
     unmount();
     expect(create).toBeCalledTimes(2);
     expect(destroy).toBeCalledTimes(2);
   });
 
-  it("should not run effect callback with an empty array as deps", () => {
+  it("should not run effect with an empty array deps", () => {
     const destroy = jest.fn();
     const create = jest.fn().mockReturnValue(destroy);
     const { rerender, unmount } = renderHook(() => {
       useUpdate(create, []);
     });
 
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     rerender();
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     rerender();
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
+
     unmount();
-    expect(create).toBeCalledTimes(0);
-    expect(destroy).toBeCalledTimes(0);
+    expect(create).not.toBeCalled();
+    expect(destroy).not.toBeCalled();
   });
 });
