ba5f3d39e73720110d0da6c0ee2dbd4763c55344
李蔚生 <971353677@qq.com>
2022-06-10 22:43:06 +0800

    test(useMount): not call sync

--- tests/useMount.test.ts
+++ tests/useMount.test.ts
@@ -1,10 +1,13 @@
-import { renderHook } from "@testing-library/react-hooks";
 import { useMount } from "@lilib/hooks";
+import { renderHook } from "@testing-library/react-hooks";
 
 describe("useMount", () => {
-  it("should run effect callback after the component is mounted", () => {
+  it("should run effect after the component is mounted", () => {
     const mock = jest.fn();
-    const { rerender, unmount } = renderHook(() => useMount(mock));
+    const { rerender, unmount } = renderHook(() => {
+      useMount(mock);
+      expect(mock).not.toBeCalled();
+    });
 
     expect(mock).toBeCalledTimes(1);
     rerender();
