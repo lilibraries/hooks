02509429a85fa539f65954b003850f35839f2a31
李蔚生 <971353677@qq.com>
2022-06-20 22:52:19 +0800

    test(useSize): add test

---
+++ tests/useSize.test.ts
@@ -0,0 +1,16 @@
+import { useSize } from "@lilib/hooks";
+import { renderHook } from "@testing-library/react-hooks";
+
+describe("useSize", () => {
+  it("should return element size", () => {
+    const element = document.createElement("div");
+    document.body.appendChild(element);
+
+    const { result } = renderHook(() => {
+      return useSize(() => element);
+    });
+
+    expect(typeof result.current.width).toBe("number");
+    expect(typeof result.current.height).toBe("number");
+  });
+});
