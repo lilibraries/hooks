f83a386552f6e178daca07c03c128b2e086d47c5
李蔚生 <971353677@qq.com>
2022-09-28 21:35:00 +0800

    chore: move examples into docs

--- docs/components/ExampleBlock/index.tsx
+++ docs/components/ExampleBlock/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from "react";
-import styles from "./ExampleBlock.module.css";
+import styles from "./index.module.css";
 
 const ExampleBlock: FC<{ children: ReactNode }> = ({ children }) => {
   return <div className={styles.exampleBlock}>{children}</div>;
