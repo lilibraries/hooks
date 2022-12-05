3b194cd9c18dc823b23dd8038046cf2a38cdd3fb
李蔚生 <971353677@qq.com>
2022-12-05 19:15:21 +0800

    chore: `assignWithDefined` add override types

--- src/utils/assignWithDefined.ts
+++ src/utils/assignWithDefined.ts
@@ -1,9 +1,36 @@
 import assignWith from "lodash/assignWith";
 
-const assignWithDefined: typeof assignWith = (base: any, ...args: any[]) => {
+function assignWithDefined<A extends object>(a: A): A;
+function assignWithDefined<A extends object, B extends object>(
+  a: A,
+  b: B
+): A & B;
+function assignWithDefined<
+  A extends object,
+  B extends object,
+  C extends object
+>(a: A, b: B, c: C): A & B & C;
+function assignWithDefined<
+  A extends object,
+  B extends object,
+  C extends object,
+  D extends object
+>(a: A, b: B, c: C, d: D): A & B & C & D;
+function assignWithDefined<
+  A extends object,
+  B extends object,
+  C extends object,
+  D extends object,
+  E extends object
+>(a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
+function assignWithDefined<T extends object>(
+  base: Partial<T>,
+  ...args: Partial<T>[]
+): T;
+function assignWithDefined(base: any, ...args: any[]) {
   return assignWith(base, ...args, (prev: any, next: any) => {
     return next === undefined ? prev : next;
   });
-};
+}
 
 export default assignWithDefined;
