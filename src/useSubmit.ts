e07d484053cacec0593e86da0133be02fd16e405
李蔚生 <971353677@qq.com>
2022-12-15 19:52:49 +0800

    feat: add `useSubmit`

--- src/useSubmit.ts
+++ src/useSubmit.ts
@@ -1,33 +1,44 @@
-import { PromiseResolve } from "./utils/types";
+import useLoad, { LoadCallback, LoadData } from "./useLoad";
 
 export interface SubmitHookOptions<Callback, Data> {
-  key?: {};
   idle?: boolean;
   fallback?: Callback;
-
   retry?: boolean;
   retryLimit?: number;
   retryInterval?: number | ((count: number) => number);
   fallbackRetry?: boolean;
   fallbackRetryLimit?: number;
   fallbackRetryInterval?: number | ((count: number) => number);
-
   onSuccess?: (data: Data) => void;
   onFailure?: (error: any) => void;
   onFinally?: () => void;
 }
 
-export declare function useSubmit<
-  Callback extends (...args: any[]) => Promise<any>,
-  Data = PromiseResolve<ReturnType<Callback>>
->(
-  callback: Callback,
-  options?: SubmitHookOptions<Callback, Data>
-): {
-  data: Data | undefined;
-  error: any;
-  submitting: boolean;
-  submit: Callback;
-  reset: () => void;
-  cancel: () => void;
+const defaultOptions = {
+  key: undefined,
+  imperative: true,
+  independent: true,
+  initialData: undefined,
+  defaultParams: undefined,
+  cacheKey: undefined,
+  staleTime: 0,
+  polling: false,
+  pollingInPageHiding: false,
+  autoReloadOnPageShow: false,
+  autoReloadOnWindowFocus: false,
+  autoReloadOnNetworkReconnect: false,
 };
+
+function useSubmit<Callback extends LoadCallback>(
+  callback: Callback,
+  options?: SubmitHookOptions<Callback, LoadData<Callback>>
+) {
+  const { load: submit, loading: submitting } = useLoad(callback, [], {
+    ...options,
+    ...defaultOptions,
+  });
+
+  return { submit, submitting } as const;
+}
+
+export default useSubmit;
